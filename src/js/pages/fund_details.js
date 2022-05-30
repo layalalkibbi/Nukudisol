0b00251a8cc14435e3a5044bc3981d541922dbff
layalalkibbi <92437075+layalalkibbi@users.noreply.github.com>
2022-05-30 12:39:13 +0300

    Add files via upload

---
+++ src/js/pages/fund_details.js
@@ -0,0 +1,19 @@
+//[Dashboard Javascript]
+
+//Project:	InvestX - Responsive Admin Template
+//Primary use:   Used only for the main dashboard (index.html)
+
+
+$(function () {
+
+  'use strict';
+	
+	
+	
+}); // End of use strict
+
+
+		
+	
+	
+	
